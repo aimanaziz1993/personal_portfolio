94528e115773d3dc4d7e941370ba42714bd32d3d
softwareU4 <softwareU4@i-smart.com.sg>
2021-12-08 16:54:17 +0800

    app project added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
+    # 'hello_world',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    ("projects", "projects/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
